77d33c7ee65753e8981289a56124fcaf9fbb5d8e
WFJ9912 <1394559650@qq.com>
2021-12-18 21:46:26 +0800

    模板方法的设计模式练习

---
+++ java12/TemplateTest.java
@@ -0,0 +1,50 @@
+package com.wufujian.java12;
+
+/**
+ * @author:gfk
+ * @create:2021/12/18
+ * @Description:
+ * @FileName:TemplateTest
+ * @History:
+ * @自定义内容:
+ */
+
+/**
+ * 模板方法设计模式（Template Method）
+ */
+public class TemplateTest {
+    public static void main(String[] args) {
+        SubTemplate t = new SubTemplate();
+        t.spendTime();
+    }
+}
+
+abstract class Template{
+    //计算某段代码执行所需要花费的时间
+    public void spendTime(){
+        long start = System.currentTimeMillis();
+        this.code();//不确定的部分、易变的部分
+        long end = System.currentTimeMillis();
+        System.out.println("花费的时间为：" + (end - start));
+    }
+    public abstract void code();
+}
+
+class SubTemplate extends Template{
+
+    @Override
+    public void code() {
+        for (int i = 2; i <= 1000; i++){
+            boolean isFlag = true;
+            for(int j = 2;j <= Math.sqrt(i);j++){
+                if(i % j == 0){
+                    isFlag = false;
+                    break;
+                }
+            }
+            if(isFlag){
+                System.out.println(i);
+            }
+        }
+    }
+}
